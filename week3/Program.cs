817411b680742ed6107d83e1399b41728dacc6e0
Hoang Vinh <hoangvinh20021503@gmail.com>
2023-08-30 14:08:37 +0700

    Coordinate

---
+++ week3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Coordinate xyz = new Coordinate();
+            xyz.Nhap();
+            xyz.Xuat();
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ week3/coordinate.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week3
+{
+    class Coordinate
+    {
+        float x;
+        float y;
+        string TenTD;
+        //get set
+        public float X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+
+        public float Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+
+        public string TenTD1
+        {
+            get { return TenTD; }
+            set { TenTD = value; }
+        }
+        //end get set
+
+        public Coordinate(float x,float y, string TenTD) {
+            this.x = x;
+            this.y = y;
+            this.TenTD = TenTD;
+        }
+        public Coordinate() {
+            float x = 0;
+            float y = 0;
+            string TenTD = "" ;
+        }
+        public void Nhap() {
+            Console.WriteLine("Ten toa do:");
+            TenTD = Console.ReadLine();
+            Console.WriteLine("Toa do X:");
+            x = float.Parse(Console.ReadLine());
+            Console.WriteLine("Toa do Y:");
+            y = float.Parse(Console.ReadLine());
+        }
+        public void Xuat() {
+            Console.WriteLine("Ten toa do: {0} \nX: {1} \nY: {2} \n",TenTD, x, y);
+        }
+    }
+}
